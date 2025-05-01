2212f106e653063a4c5c6501670df1467233e99a
najongjine <najongjin3@hotmail.com>
2025-05-01 12:56:48 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import { UnauthorizedExceptionFilter } from './auth/strategies/customUnauthorizedException';
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   /** 내가만든 커스텀 Filter 전역 적용하기  */
   app.useGlobalFilters(new UnauthorizedExceptionFilter());
