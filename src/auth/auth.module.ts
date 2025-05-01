30b0a1af9ce677ecff32fb143a02df3282314cc6
najongjine <najongjin3@hotmail.com>
2025-05-01 12:26:43 +0900

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { SessionSerializer } from './strategies/session.serializer';
 
